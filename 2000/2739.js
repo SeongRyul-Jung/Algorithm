8c2f2d6649d67d8703fdb9cb1c32e33bca9b3e92
SR-Jung <westeeven0612@gmail.com>
2022-11-15 12:45:59 +0900

    2739

---
+++ 2000/2739.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for(let i=1; i<=9; i++){
+  console.log(`${+input} * ${i} = ${+input*i}`)
+}
